a965b10b7d075dda985ed09cd01e67df8dea8f2a
djones321 <32394137+djones321@users.noreply.github.com>
2021-01-29 23:53:29 -0600

    Initial Upload

---
+++ input-example-djones321/InputExample/InputExampleGame.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace InputExample
+{
+    public class InputExampleGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Ball[] balls;
+        private InputManager inputManager;
+
+        
+        /// <summary>
+        /// Constructs the game
+        /// </summary>
+        public InputExampleGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+        }
+
+        /// <summary>
+        /// Initializes the game
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            balls = new Ball[] {
+                new Ball(this, Color.Red) { Position = new Vector2(250, 200) },
+                new Ball(this, Color.Green) { Position = new Vector2(350, 200) },
+                new Ball(this, Color.Blue) { Position = new Vector2(450, 200) }
+            };
+
+            inputManager = new InputManager();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Loads game content
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            foreach (Ball b in balls) b.LoadContent();
+        }
+
+        /// <summary>
+        /// Updates the game
+        /// </summary>
+        /// <param name="gameTime">The game time</param>
+        protected override void Update(GameTime gameTime)
+        {
+            inputManager.Update(gameTime);
+
+            if (inputManager.Exit) Exit();
+
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || currentKeyboardState.IsKeyDown(Keys.Escape))
+            //Exit();
+
+            // TODO: Add your update logic here
+
+            if (inputManager.Warp) balls[1].Warp();
+            balls[0].Position += inputManager.Direction;
+            balls[2].Position -= inputManager.Direction;
+
+
+            
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Draws the game
+        /// </summary>
+        /// <param name="gameTime">The game time</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            foreach (Ball b in balls) b.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ input-example-djones321/InputExample/InputManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace InputExample
+{
+    class InputManager
+    {
+        KeyboardState currentKeyboardState;
+        KeyboardState priorKeyboardState;
+        MouseState currentMouseState;
+        MouseState previousMouseState;
+        GamePadState currentGamePadState;
+        GamePadState previousGamePadState;
+
+        public Vector2 Direction { get; private set; }
+
+        public bool Warp { get; private set; }
+
+        public bool Exit { get; private set; }
+
+        public void Update(GameTime gameTime)
+        {
+            priorKeyboardState = currentKeyboardState;
+            currentKeyboardState = Keyboard.GetState();
+
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+
+            previousGamePadState = currentGamePadState;
+            currentGamePadState = GamePad.GetState(0);
+
+            if(currentGamePadState.Buttons.Back == ButtonState.Pressed || currentKeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Exit = true;
+            }
+
+
+            #region directional input
+
+            //Gamepad
+            Direction = currentGamePadState.ThumbSticks.Left * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            /*
+            //Mouse
+            Direction = new Vector2(
+                currentMouseState.X * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds,
+                currentMouseState.Y * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds
+            );*/
+            
+
+            //Keyboard
+            if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+                currentKeyboardState.IsKeyDown(Keys.A))
+            {
+                Direction += new Vector2(-100 * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+                currentKeyboardState.IsKeyDown(Keys.D))
+            {
+                Direction += new Vector2(100 * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+                currentKeyboardState.IsKeyDown(Keys.W))
+            {
+                Direction += new Vector2(0, -100 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+                currentKeyboardState.IsKeyDown(Keys.S))
+            {
+                Direction += new Vector2(0, 100 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            #endregion
+
+            #region Warp
+            
+            Warp = false;
+            if (currentKeyboardState.IsKeyDown(Keys.Space) &&
+                priorKeyboardState.IsKeyUp(Keys.Space))
+            {
+                Warp = true;
+            }
+
+            if (currentGamePadState.IsButtonDown(Buttons.A) &&
+                previousGamePadState.IsButtonUp(Buttons.A))
+            {
+                Warp = true;
+            }
+
+            #endregion
+        }
+
+    }
+}
